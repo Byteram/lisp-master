7d27ca883388649e79226be042d1391ec95b0ea7
victorkolis <victorkolis@gmail.com>
2024-05-06 13:03:38 -0300

    small change adding the documentation string to the function

--- small-programs/01-greetings-stranger.lisp
+++ small-programs/01-greetings-stranger.lisp
@@ -1,4 +1,5 @@
 (defun greet-user ()
+  "Greets user and asks for name and reply back" ; Documentation string/text
   (princ "What is your name? ")  ; Outputs the prompt
   (finish-output)                ; Ensure the prompt is visible immediately
   (let ((name (read-line)))
